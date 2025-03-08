7a1dac2f502ac5dc0c3f76991e587adf0e7aec81
KushalAgrawal30 <kushalagrawal3011@gmail.com>
2025-03-09 04:31:20 +0530

    added icons

--- drawing-board/src/Canvas.jsx
+++ drawing-board/src/Canvas.jsx
@@ -1,5 +1,7 @@
 import {ReactSketchCanvas} from "react-sketch-canvas";
 import {ChangeEvent, useRef, useState} from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPaintbrush, faEraser, faRotateLeft, faRotateRight, faArrowsRotate, faBroom, faDownload} from '@fortawesome/free-solid-svg-icons'
 
 function Canvas(){
     const canvasRef = useRef(null);
@@ -129,17 +131,17 @@ function Canvas(){
         <div className="container">
             <h1>AI Sketch Book</h1>
             <div className="tool-section">
-                <button type="button" disabled={!eraseMode} onClick={handlePenClick}>Pen</button>
-                <button type="button" disabled={eraseMode} onClick={handleEraser}>Eraser</button>
+                <button type="button" disabled={!eraseMode} title="Pen" onClick={handlePenClick}><FontAwesomeIcon icon={faPaintbrush} /></button>
+                <button type="button" disabled={eraseMode} title="Erase" onClick={handleEraser}><FontAwesomeIcon icon={faEraser} /></button>
                 <label htmlFor="strokeWidth">Stroke Width </label>
                 <input type="range" disabled={eraseMode} id="strokeWidth" value={strokeWidth} onChange={handleStrokeWidth} min="1" max="20" step="1"/>
                 <label htmlFor="eraseWidth">Eraser Width </label>
                 <input type="range" disabled={!eraseMode} id="eraseWidth" value={eraseWidth} onChange={handleEraserWidth} min="1" max="20" step="1"/>
-                <button type="button" onClick={handleUndoClick}>Undo</button>
-                <button type="button" onClick={handleRedoClick}>Redo</button>
-                <button type="button" onClick={handleClearClick}>Clear</button>
-                <button type="button" onClick={handleResetClick}>Reset</button>
-                <button type="button" onClick={handleSave}>Save</button>
+                <button type="button" title="Undo" onClick={handleUndoClick}><FontAwesomeIcon icon={faRotateLeft} /></button>
+                <button type="button" title="Redo" onClick={handleRedoClick}><FontAwesomeIcon icon={faRotateRight} /></button>
+                <button type="button" title="Clear" onClick={handleClearClick}><FontAwesomeIcon icon={faBroom} /></button>
+                <button type="button" title="Reset" onClick={handleResetClick}><FontAwesomeIcon icon={faArrowsRotate} /></button>
+                <button type="button" title="Save" onClick={handleSave}><FontAwesomeIcon icon={faDownload} /></button>
             </div>
             <div className="color-section">
                 <label htmlFor="strokeColor">Stroke Color </label>
